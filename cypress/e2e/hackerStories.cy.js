afa0ff5510405271cc203558facca013a6471a3a
Arthur Gomes <arthursgmes@gmail.com>
2025-01-21 22:33:08 -0300

    lendo localstorage do navegador

--- cypress/e2e/hackerStories.cy.js
+++ cypress/e2e/hackerStories.cy.js
@@ -50,11 +50,17 @@ describe('Hacker Stories', () => {
 
       cy.wait('@getNewTermStories')
 
+      cy.getLocalStorage('search')
+        .should('be.equal', newTerm)
+
       cy.get(`button:contains(${initialTerm})`)
         .should('be.visible')
         .click()
 
       cy.wait('@getStories')
+
+      cy.getLocalStorage('search')
+        .should('be.equal', initialTerm)
 
       cy.get('.item').should('have.length', 20)
       cy.get('.item')
@@ -254,6 +260,9 @@ describe('Hacker Stories', () => {
           .type(`${newTerm}{enter}`)
   
         cy.wait('@getStories')
+
+        cy.getLocalStorage('search')
+          .should('be.equal', newTerm)
   
         cy.get('.item').should('have.length', 2)
         cy.get(`button:contains(${initialTerm})`)
@@ -270,6 +279,9 @@ describe('Hacker Stories', () => {
   
         cy.wait('@getStories')
   
+        cy.getLocalStorage('search')
+          .should('be.equal', newTerm)
+
         cy.get('.item').should('have.length', 2)
         cy.get(`button:contains(${initialTerm})`)
           .should('be.visible')
@@ -288,15 +300,21 @@ describe('Hacker Stories', () => {
           ).as('getRandomStories')
   
           Cypress._.times(6, () => {
+            const randomWord = faker.random.word()
+
             cy.get('#search')
               .clear()
-              .type(`${faker.random.word()}{enter}`)
+              .type(`${randomWord}{enter}`)
               cy.wait('@getRandomStories')
+              cy.getLocalStorage('search')
+                .should('be.equal', randomWord)
           })
-  
-  
-          cy.get('.last-searches button')
-            .should('have.length', 5)
+
+          cy.get('.last-searches')
+            .within(() => {
+              cy.get('button')
+                .should('have.length', 5)
+            })
         })
       })
     })
